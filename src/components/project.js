4ab2f5039952d774d156b45701be74cc07eed639
Liem Phan <minhliemphp@gmail.com>
2017-10-30 08:14:28 +0700

    update

--- src/components/project.js
+++ src/components/project.js
@@ -10,16 +10,19 @@ export default class Project extends Component{
             showProjectItem: 3,
             expanded: false
         }
-
-        this.onHandleShowitem = this.onHandleShowitem.bind(this)
+        // BINDING
+        this.onHandleShowitem = this.onHandleShowitem.bind(this);
+        this.onHandleModal = this.onHandleModal.bind(this);
     }
 
     componentDidMount(){
         this.getProjectList();
+
     }
 
     getProjectList(){
         CommonModel.getProjects().then((data) => {
+            // console.log(data.data);
            this.setState({
                projects: data.data
            })
@@ -27,27 +30,54 @@ export default class Project extends Component{
     }
     /*EVENT HANDLE*/
     onHandleShowitem(event){
+        if(this.state.showProjectItem === 3){
+            this.setState({
+                showProjectItem: this.state.projects.result.length,
+                expanded: true
+            })
+        }else{
+            this.setState({
+                showProjectItem: 3,
+                expanded: false
+            })
+        }
+
+    }
+
+    onHandleModal(event){
 
     }
 
     render(){
+        this.state.projects !== null && console.log(this.state.projects.result.slice(0, this.state.showProjectItem));
         return(
             <section id="portfolio">
                 <div className="container">
                     <h2 className="text-center">Projects</h2>
                     <hr className="star-primary"/>
                     <div className="row">
-                        <div className="col-sm-4  portfolio-item">
-
-                            <a className="portfolio-link" href="#portfolioModal1" data-toggle="modal">
-                                <div className="caption">
-                                    <div className="caption-content">
-                                        <i className="fa fa-search-plus fa-3x"></i>
+                        {
+                            this.state.projects !== null && this.state.projects.result.length > 0 ? (
+                                this.state.projects.result.slice(0, this.state.showProjectItem).map( (rs, key)=>
+                                    <div className="col-sm-4  portfolio-item" key={key} >
+                                        <a className="portfolio-link" onClick={this.onHandleModal} project={rs }>
+                                            <div className="caption">
+                                                <div className="caption-content">
+                                                    <i className="fa fa-search-plus fa-3x"></i>
+                                                </div>
+                                            </div>
+                                            <img className="img-fluid" src="img/portfolio/cabin.png" alt=""/>
+                                        </a>
                                     </div>
+                                )
+                            ) : (
+                                <div className="col-sm-12 portfolio-item text-center">
+                                    <p>Chưa có dữ liệu</p>
                                 </div>
-                                <img className="img-fluid" src="img/portfolio/cabin.png" alt=""/>
-                            </a>
-                        </div>
+                            )
+                        }
+
+
                     </div>
                     {
                         this.state.expanded ? (
@@ -62,7 +92,7 @@ export default class Project extends Component{
                             <div className="row">
                                 <div className="col-sm-12">
                                     <div className="btn-container text-center">
-                                        <button type="button" className="btn btn-primary" onClick={this.this.onHandleShowitem}>Show More</button>
+                                        <button type="button" className="btn btn-primary" onClick={this.onHandleShowitem}>Show More</button>
                                     </div>
                                 </div>
                             </div>
